102f912d49217407bb8a7abef20ec571b7d461ae
jcgutierrez02 <jcgutierrez02@gmail.com>
2023-08-18 15:52:45 +0200

    Modificación congig_BD.php para acceso a db4free.net

--- config/config_BD.php
+++ config/config_BD.php
@@ -10,19 +10,22 @@
     */
 
      // Parámetros acceso a una BD db4free
-     /*
+     
     define('HOST', 'db4free.net'); 
     define('BD', 'iesdaw');
     define('USER', 'jcarlos');
     define('PASSWORD', 'Jcarl0sJcarl0s');
     define('CHARSET', 'utf8');
-    */
+    
 
+    // Parámetros acceso a al contenedor
+    /*
     define('HOST', 'mariadb'); 
     define('BD', 'ventas');
     define('USER', 'daw');
     define('PASSWORD', 'dawPassw0rd');
     define('CHARSET', 'utf8');
+    */
 
 
 ?>
